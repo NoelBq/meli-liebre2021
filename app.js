d785db8c75c52cb7a9bfd66b3f3a8347a71ab748
DESKTOP-KQT5V53\Noelia <noeliabaquerostaci@gmail.com>
2021-08-11 22:37:24 -0300

    lal

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const staticFolder = path.resolve(__dirname, 'public');
+const staticFolder = path.resolve(__dirname, './public');
 app.use(express.static(staticFolder));
 
 app.get('/', (req, res) => {
