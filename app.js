38bb40ae87fae70c3b25f776936f301d44d2336d
DESKTOP-KQT5V53\Noelia <noeliabaquerostaci@gmail.com>
2021-08-11 22:18:30 -0300

    port changed

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+const staticFolder = path.resolve(__dirname, 'public');
+app.use(express.static(staticFolder));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+
+app.listen(PORT, () => {
     console.log('server running')
 })
